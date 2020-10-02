b6a5b9a8a105245277cc1bddd68a5a6254fa5fca
Heisenberg-Vardan <vardannarula@gmail.com>
2020-10-02 18:08:37 +0530

    Added Vertical order traversal of a Binary Tree

---
+++ C++/Data Structure/Binary Trees/Vertical order Traversal of Binary Tree.cpp
@@ -0,0 +1,41 @@
+#include iostream
+#include vector
+using namespace std;
+
+int main() {
+	code
+	
+	int t;
+	cint;
+	int n[t];
+	vectorint a[t];
+	for(int j=0;jt;j++){
+	    cinn[j];
+	    for(int k=0;kn[j];k++){
+	        int x;
+	        cinx;
+	        a[j].push_back(x);
+	    }
+	    
+	}
+	
+	int i=0;
+	while(it){
+	    vectorint v;
+	    for(int j=0;jn[i]-1;j++){
+	        int sum=a[i].at(j);
+	        for(int k=0;kn[i];k++){
+	            sum=sum+a[i].at(k);
+	            v.push_back(sum);
+	        }
+	    }
+	    int max=v.at(0);
+	    int p=0;
+	    for(int j=0;jv.size();j++){
+	        if(a[i].at(j)max){
+	            max=a[i].at(j);
+	        }
+	    }
+	    coutmaxendl;
+	}
+}

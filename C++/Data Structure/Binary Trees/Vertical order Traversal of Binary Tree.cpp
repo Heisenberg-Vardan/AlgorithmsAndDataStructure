a271ac14f441055a7c1da42699951cfc3d2a497a
Heisenberg-Vardan <vardannarula@gmail.com>
2020-10-02 18:27:53 +0530

    Added Vertical order traversal of a Binary Tree

--- C++/Data Structure/Binary Trees/Vertical order Traversal of Binary Tree.cpp
+++ C++/Data Structure/Binary Trees/Vertical order Traversal of Binary Tree.cpp
@@ -1,41 +1,58 @@
-#include iostream
-#include vector
+/*Title - Vertical Order Traversal of a Binary Tree
+
+Prints the vertical order traversal of a binary tree
+
+Time complexity -  0(n)
+
+*/
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-	code
+int main() 
+{
+	
 	
 	int t;
-	cint;
+	cin>>t;
 	int n[t];
-	vectorint a[t];
-	for(int j=0;jt;j++){
-	    cinn[j];
-	    for(int k=0;kn[j];k++){
+	vector<int> a[t];
+	for(int j=0;j<t;j++)
+	{
+	    cin>>n[j];
+	    for(int k=0;k<n[j];k++)
+	    {
 	        int x;
-	        cinx;
+	        cin>>x;
 	        a[j].push_back(x);
 	    }
 	    
 	}
 	
 	int i=0;
-	while(it){
-	    vectorint v;
-	    for(int j=0;jn[i]-1;j++){
+	while(i<t)
+	{
+	    vector<int> v;
+	    for(int j=0;j<n[i]-1;j++)
+	       {
 	        int sum=a[i].at(j);
-	        for(int k=0;kn[i];k++){
+	        for(int k=0;k<n[i];k++)
+		  {
 	            sum=sum+a[i].at(k);
 	            v.push_back(sum);
+	           }
 	        }
-	    }
 	    int max=v.at(0);
 	    int p=0;
-	    for(int j=0;jv.size();j++){
-	        if(a[i].at(j)max){
+	    for(int j=0;j<v.size();j++)
+	        {
+	        if(a[i].at(j)>max)
+		   {
 	            max=a[i].at(j);
+	            }
 	        }
-	    }
-	    coutmaxendl;
+	    cout<<max<<endl;
 	}
+	return 0;
 }
